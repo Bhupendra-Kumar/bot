a0c743ecee5d218e783b528e169ec1d928707a88
Bhupendra-Kumar <Bhupeekr.77777@gmail.com>
2019-12-13 18:11:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,35 @@
-<?php
+<?php 
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-//Process only when method is post
-if($method == "POST"){
+// Process only when method is POST
+if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-	
+
 	$text = $json->result->parameters->text;
-	
-	/*switch ($text) {
+
+	switch ($text) {
 		case 'hi':
-		$speech = "Hi, Nice to meet you";
-		break;
-		
+			$speech = "Hi, Nice to meet you";
+			break;
+
 		case 'bye':
-		$speech = "Bye, good night";
-		break;
-		
+			$speech = "Bye, good night";
+			break;
+
 		case 'anything':
-		$speech = "Yes, you can type anything here.";
-		break;
+			$speech = "Yes, you can type anything here.";
+			break;
 		
 		default:
-		$speech = "Sorry, I didn't get that. Please ask me something else.";
-		break;
-	}*/
-	
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
+	}
+
 	$response = new \stdClass();
-	$response->speech = $text;
-	$response->displayText = $text;
+	$response->speech = $speech;
+	$response->displayText = $speech;
 	$response->source = "webhook";
 	echo json_encode($response);
 }
@@ -37,4 +37,5 @@ else
 {
 	echo "Method not allowed";
 }
+
 ?>
