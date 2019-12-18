4ecbf54240bf877f4c62e8fe6f418fd2eaca97d9
Bhupendra-Kumar <Bhupeekr.77777@gmail.com>
2019-12-18 18:56:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if($method == 'POST'){
 
 	$response = new \stdClass();
 	$response->speech = $requestBody;
-	$response->displayText = $json["queryResult"];
+	$response->displayText = $json["queryResult"]["parameters"];
 	$response->source = "webhook";
 	echo json_encode($response);
 }
