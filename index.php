83489dd80cf0c88db3eb155f5f481f5c8907cc1a
Bhupendra-Kumar <Bhupeekr.77777@gmail.com>
2019-12-18 17:28:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if($method == 'POST'){
 	$requestBody = file_get_contents("php://input");
 	$json = json_decode($requestBody);
 
-	$text = $json->result->queryText;
+	$text = $json->result->parameters->text;
 	switch ($text) {
 		case 'hi':
 			$speech = "Hi, Nice to meet you";
