e8396ffb20280bd6cfbdcc2a29fe8d65e43f60f6
Bhupendra-Kumar <Bhupeekr.77777@gmail.com>
2019-12-19 12:08:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if($method == 'POST'){
 	$response = new \stdClass();
 	$response->speech = $speech;
 	$response->displayText = $speech;
-	$response->source = $json["queryResult"]["source"];
+	$response->source = $json;
 	echo json_encode($response);
 }
 else
